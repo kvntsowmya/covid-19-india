1afe62a23a1af41ad4653558e19ef99841e42ba6
kvnt sowmya <kvntsowmya@gmail.com>
2021-04-06 13:42:09 +0000

    covid india

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const pathLoc = path.join(__dirname, "covid19India.db");
+let database = null;
+
+const InitializeDBAndServer = () => {
+  try {
+    database = open({
+      filename: pathLoc,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server running at https://localhost:3000/");
+    });
+  } catch (error) {
+    console.log(`error occured: ${error.message}`);
+  }
+};
+
+InitializeDBAndServer();
+
+app.get("/states/", async (request, response) => {
+  const getAllStatesQuery = `
+    select * from state`;
+
+  const databaseResponse = await database.all(getAllStatesQuery);
+  response.send(databaseResponse);
+});
