e2211440c6df5b7c583ddd5c0132a4f15db65b7c
kvntsowmya <34996721+kvntsowmya@users.noreply.github.com>
2021-04-09 14:31:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,3 +32,39 @@ app.get("/states/", async (request, response) => {
   const databaseResponse = await database.all(getAllStatesQuery);
   response.send(databaseResponse);
 });
+
+app.get("/states/:stateId/", async (request, response) => {
+  const { stateId } = request.params;
+
+  const getRequiredState = `
+  select state_id as stateId, state_name as stateName, population 
+  from state 
+  where state_id = ${stateId};`;
+
+  const requiredState = await database.get(getRequiredState);
+  response.send(requiredState);
+});
+
+app.post("/districts/", async (request, response) => {
+  const districtDetails = request.body;
+  const {
+    districtName,
+    stateId,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = districtDetails;
+
+  const postDetails = `
+  insert into district (district_name, state_id,
+  cases, cured, active, deaths)
+  values (${districtName},
+  ${stateId},
+  ${cases},
+  ${cured},
+  ${active},
+  ${deaths});`;
+  const postResponse = await database.run(postDetails);
+  response.send("District Successfully Added");
+});
